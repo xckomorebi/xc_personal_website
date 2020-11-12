d182b008e6ab6ac74630eea2ea727bf95231cd7b
Chen Xu <xuchen.prog@bytedance.com>
2020-11-12 15:47:52 +0800

    update

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     
 
     def __str__(self):
-        return f"{self.user.username}\'s Profile"
+        return f"{self.user.username} Profile"
 
     # def save(self):
         # super().save()
